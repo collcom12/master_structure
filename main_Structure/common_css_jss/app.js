eaadb75c7e42076f2efaeadda9c51ab706a12ceb
collcom12 <73544397+collcom12@users.noreply.github.com>
2020-10-29 17:06:49 +0530

    Add files via upload

---
+++ main_Structure/common_css_jss/app.js
@@ -0,0 +1,20 @@
+//Common js file
+
+
+/*Pooja|Mansi*/
+
+/*Chetan|Shanwaz*/
+
+/*Nikki| Himanshi*/
+
+/*Utkarsh| Lakshya*/
+
+/*Prince| Adarsh*/
+
+/*Gazal | Saurabh*/
+
+/*Gazal |Saurabh|Blogging*/
+
+/*Vineet|Aman|Pratima*/
+
+/*Footer*/
